77bb04b8eb7c2e34a047f0f556c9230d2045b4ad
Tanzil <mdtanzil858@gmail.com>
2021-09-16 16:36:49 +0600

    todo ui added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const settingsMenu = document.querySelector(".user-settings");
+
+function userSettings () {
+    settingsMenu.classList.toggle("user-settings-height");
+}
---
+++ js/vue.js
@@ -0,0 +1,6 @@
+var app2 = new Vue({
+    el: '#vue_app',
+    created: function () {
+        console.log('created');
+    }
+  })
