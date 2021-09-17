8aae9b46c214bdf99ad506a9625190e3dda582a6
Tanzil <mdtanzil858@gmail.com>
2021-09-17 16:26:11 +0600

    bug

--- js/vue.js
+++ js/vue.js
@@ -1,6 +1,41 @@
 var app2 = new Vue({
     el: '#vue_app',
     created: function () {
-        console.log('created');
+        console.log(this.todo_list);
+    },
+    data: function () {
+        return {
+            todo_title: '',
+            todo_list: [
+                {
+                    checked: false,
+                    title: 'test1'
+                },
+                {
+                    checked: false,
+                    title: 'test2'
+                },
+                {
+                    checked: false,
+                    title: 'test3'
+                },
+                {
+                    checked: false,
+                    title: 'test4'
+                },
+            ]
+        }
+    },
+    methods: {
+        show_todo_title_value: function () {
+            console.log(this.todo_title);
+        },
+        add_new_data: function() {
+            let data = {
+                checked: false,
+                title: this.todo_title
+            }
+            this.todo_list.push(data)
+        }
     }
   })
