05a8a96a9d6fa9fe5c0dc4ca031b84fd70963040
Tanzil <mdtanzil858@gmail.com>
2021-09-18 00:04:48 +0600

    sidebar added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 const settingsMenu = document.querySelector(".user-settings");
+const sidebarToggle = document.querySelector(".sidebar_toggle");
+const navClose = document.querySelector(".nav-close");
+const overlay = document.querySelector(".overlay");
 
 function userSettings () {
     settingsMenu.classList.toggle("user-settings-height");
 }
+
+sidebarToggle.addEventListener('click', function (){
+    document.querySelector(".sidebar").setAttribute('style', 'display:block');
+    overlay.setAttribute('style', 'opacity:0.1')
+})
+
+navClose.addEventListener('click', function (){
+    document.querySelector(".sidebar").setAttribute('style', 'display:none');
+    overlay.setAttribute('style', 'opacity:0')
+})
+overlay.addEventListener('click', function (){
+    document.querySelector(".sidebar").setAttribute('style', 'display:none');
+    overlay.setAttribute('style', 'opacity:0')
+})
